084b4fb647395d67605a86e1ab371d6057fa6138
ajoshi11 <atul.uhc@gmail.com>
2025-11-12 07:56:36 +0530

    Update ConnectRenderDB.py

--- ConnectRenderDB.py
+++ ConnectRenderDB.py
@@ -12,11 +12,11 @@ def main():
     st.title("Render PostgreSQL DB Output")
 
     # ⚡ Hardcoded DB Connection Info
-    host="dpg-d2l8ebf5r7bs73de15vg-a.oregon-postgres.render.com"
+    host="dpg-d3t5f4mmcj7s73fgm5s0-a.oregon-postgres.render.com"
     port=5432
-    dbname="slothdb"
-    user="slothdb_user"
-    password="q8tZEErzUYiafyWgxmLmILapiLWH4I3O"
+    dbname="slothdb3"
+    user="slothdb3_user"
+    password="uWqtzvpmgNXMs9dWLZuEKDu4mH3RlxAR"
 
     if st.button("Fetch Tables"):
         try:
